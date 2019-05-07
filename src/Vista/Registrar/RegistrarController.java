6a0f9a7b446f5cbf86d3fd8f29e78580ef306724
34791843l <34791843l@2A1PC23.ad.fpmislata.com>
2019-05-07 12:54:53 +0200

    1prueba 2prueba

--- src/Vista/Registrar/RegistrarController.java
+++ src/Vista/Registrar/RegistrarController.java
@@ -12,6 +12,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -27,6 +32,36 @@ public class RegistrarController implements Initializable {
     @FXML
     private Button aceptarBT;
     private Connection conn;
+    @FXML
+    private AnchorPane Ventana1;
+    @FXML
+    private TextField nickTF;
+    @FXML
+    private TextField ContraPF;
+    @FXML
+    private TextField nombreTF;
+    @FXML
+    private TextField apellidosTF;
+    @FXML
+    private TextField dniTF;
+    @FXML
+    private TextField telefonoTF;
+    @FXML
+    private TextField direccionTF;
+    @FXML
+    private TextField emailTF;
+    @FXML
+    private PasswordField repContraPF;
+    @FXML
+    private TextField fecNacTF;
+    @FXML
+    private RadioButton clienteRB;
+    @FXML
+    private ToggleGroup rolUsuRB;
+    @FXML
+    private RadioButton AdminRB;
+    @FXML
+    private Button salirBT;
 
     public void setConn(Connection conn) {
         this.conn = conn;
@@ -48,6 +83,14 @@ public class RegistrarController implements Initializable {
         Stage stage = (Stage) this.aceptarBT.getParent().getScene().getWindow();   //Identificamos la ventana (Stage) 
         stage.close();
         //////// fin cerrar ventana ////
+    }
+
+    @FXML
+    private void mostrarRepContra(MouseEvent event) {
+    }
+
+    @FXML
+    private void salir(ActionEvent event) {
     }
 
 }
