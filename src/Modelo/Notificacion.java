625c399b92c7060a8a8d27701f655f1ff5f2546d
24331475m <24331475m@2A1PC22.ad.fpmislata.com>
2019-05-09 12:54:45 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/Modelo/Notificacion.java
+++ src/Modelo/Notificacion.java
@@ -1,26 +1,86 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.util.Duration;
 import org.controlsfx.control.Notifications;
 
-/**
- *
- * @author 20857464y
- */
+
 public class Notificacion {
+    
+    public static void info(String titulo, String mensaje){
+        Notifications notification = Notifications.create()
+        .title(titulo)
+        .text(mensaje)
+        .graphic(null)
+        .hideAfter(Duration.seconds(10))
+        .position(Pos.TOP_CENTER)
+        .onAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                System.out.println("Not supported yet.");
+            }
 
-//    Notifications notificacion;
-
-    public void error(String titulo, String mensaje) {
-        Notifications.create()
-            .text(mensaje)
-            .title(titulo)
-            .showInformation();
+        });
+        
+        notification.showInformation();
     }
+    
+    public static void confirm(String titulo, String mensaje){
+        Notifications notification = Notifications.create()
+        .title(titulo)
+        .text(mensaje)
+        .graphic(null)
+        .hideAfter(Duration.seconds(5))
+        .position(Pos.TOP_RIGHT)
+        .onAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                System.out.println("Not supported yet.");
+            }
 
+        });
+        
+        notification.showConfirm();
+    }
+    
+    public static void error(String titulo, String mensaje) {
+        Notifications notification = Notifications.create()
+        .title(titulo)
+        .text(mensaje)
+        .graphic(null)
+        .hideAfter(Duration.seconds(5))
+        .position(Pos.CENTER_RIGHT)
+        .onAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                System.out.println("Not supported yet.");
+            }
+
+        });
+        
+        notification.showError();
+    }
+    
+    public static void alert(String titulo, String mensaje) {
+        Notifications notification = Notifications.create()
+        .title(titulo)
+        .text(mensaje)
+        .graphic(null)
+        .hideAfter(Duration.seconds(5))
+        .position(Pos.BOTTOM_RIGHT)
+        .onAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                System.out.println("Not supported yet.");
+            }
+
+        });
+        
+        notification.showWarning();   
+    }
+       
+    
 }
