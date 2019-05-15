cfa0c93b36e76166055f95494f897e1bf7701880
joseRod <joserodriguezballester@gmail.com>
2019-05-15 21:15:40 +0200

    Merge origin/master
    Conflicts:
    	src/Vista/Usuario/UsuarioController.java

--- src/Vista/Transporte/TransporteController.java
+++ src/Vista/Transporte/TransporteController.java
@@ -26,24 +26,20 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-
 public class TransporteController implements Initializable {
 
     @FXML
     private ImageView imagTarj;
-    
-    
+
     private static GestionBD gestion;
     private Connection conn;
     private TransportesDAO transDAO;
     private Transporte transporte;
     private Notificacion not;
-    
-    
+
     private List<Transporte> listaTransporte = new ArrayList<>();
     @FXML
     private TreeView<String> informacionTV;
-    
 
     public void setConn(Connection conn) {
         this.conn = conn;
@@ -51,55 +47,80 @@ public class TransporteController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       transDAO = new TransportesDAO(gestion);
-         //CREACION NODO DAW
-        TreeItem<String> mapasTreeItem = new TreeItem<>("Mapas");
-        mapasTreeItem.getChildren().add(new TreeItem<>("Autobuses"));
-        mapasTreeItem.getChildren().add(new TreeItem<>("Metro"));
-        mapasTreeItem.getChildren().add(new TreeItem<>("Ferry"));
+        transDAO = new TransportesDAO(gestion);
+        //CREACION NODO DAW
+        TreeItem<String> mapasItem = new TreeItem<>("Mapas");
+//        mapasItem.getChildren().add(new TreeItem<>("Autobuses"));
+        TreeItem<String> autobuses = new TreeItem<>("Autobuses");
+        autobuses.getChildren().add(new TreeItem("Turistico"));
+        autobuses.getChildren().add(new TreeItem("Nocturnos"));
+        mapasItem.getChildren().add(autobuses);
+        mapasItem.getChildren().add(new TreeItem<>("Metro"));
+        mapasItem.getChildren().add(new TreeItem<>("Ferry"));
+        mapasItem.getChildren().add(new TreeItem<>("Tranvia"));
 //        Node icono = new ImageView(new Image(getClass().getResourceAsStream("/imagenes/icono.png")));
 //        daw.setGraphic(icono);
 
         //CREACION NODO ASIR
-        TreeItem<String> hotelTreeItem = new TreeItem<>("Hoteles");
-        hotelTreeItem.getChildren().add(new TreeItem<>("hotel1"));
-        hotelTreeItem.getChildren().add(new TreeItem<>("hotel2"));
+        TreeItem<String> hotelTreeItem = new TreeItem<>("Centro");
+        hotelTreeItem.getChildren().add(new TreeItem<>("Restaurantes"));
+//        hotelTreeItem.getChildren().add(new TreeItem<>("hotel2"));
 
         //CREACION NODO SMR
 //        TreeItem<String> smr = new TreeItem<>("SMR");
 //        smr.getChildren().add(new TreeItem<>("Implantacion de aplicaciones web"));
 //        smr.getChildren().add(new TreeItem<>("ofimatica"));
-
         //CREACION DEL TREEITEM INICIAL PARA RELLENAR EL TREEVIEW ciclos
         TreeItem<String> informacionItem = new TreeItem<>("inicio arbol");
         informacionTV.setRoot(informacionItem);
 //        ciclos.setShowRoot(false);  //OCULTAR EL ROOT
 
 //ASOCIAR AL NODO INFORMATICA LOS NODOS DE CADA CICLO
-        informacionItem.getChildren().addAll(mapasTreeItem, hotelTreeItem);
+        informacionItem.getChildren().addAll(mapasItem, hotelTreeItem);
 //OTRA FORMA DE ASOCIAR AL NODO INFORMATICA LOS NODOS DE CADA CICLO
 //        informatica.getChildren().add(daw);
 //        informatica.getChildren().add(asir);
 //        informatica.getChildren().add(smr);
 
-    //LO MOSTRAMOS DEPLEGADO
+        //LO MOSTRAMOS DEPLEGADO
         informacionItem.setExpanded(true);
 //           imagTarj.setImage(new Image("Imagenes/fondoActividad.jpg" ));
+informacionTV.getSelectionModel().selectedItemProperty().addListener((v, oldValue, newValue) -> { 
+// Body would go here
+seleccionar();
+});
     }
-    @FXML
-    private void seleccionar(MouseEvent event) {
+
+    private void seleccionar() {
         TreeItem<String> nodoSeleccionado = informacionTV.getSelectionModel().getSelectedItem();
         if (nodoSeleccionado != null) {
             String modulo = nodoSeleccionado.getValue();
-            if (modulo.equals("Autobuses")){
-//                 Image img = new Image("Imagenes/fondoActividad.jpg");
-                 imagTarj.setImage(new Image("Imagenes/fondoActividad.jpg" ));
-//        ImageView imagev = new ImageView(img);
-//               imagTarj.setImage(img);
+            switch (modulo) {
+                case "Turistico":
+                    imagTarj.setImage(new Image("Imagenes/informacion/autobus-turistico.jpg"));
+                    break;
+                case "Nocturnos":
+                    imagTarj.setImage(new Image("Imagenes/informacion/autobusnocturno.gif"));
+                    break;
+                case "Metro":
+                    imagTarj.setImage(new Image("Imagenes/informacion/metro_lineas_mapa.jpg"));
+                    break;
+                case "Ferry":
+                    imagTarj.setImage(new Image("Imagenes/informacion/ferry.jpg"));
+                    break;
+                case "Tranvia":
+                    imagTarj.setImage(new Image("Imagenes/informacion/plano-tranvias.png"));
+                    break;
+                case "Centro":
+                    imagTarj.setImage(new Image("Imagenes/informacion/trans4.png"));
+                    break;
+                case "Restaurantes":
+                    imagTarj.setImage(new Image("Imagenes/informacion/amsterdam-restaurantes-mapa.jpg"));
+                    break;
+
             }
         }
 
     }
-   
 
 }
