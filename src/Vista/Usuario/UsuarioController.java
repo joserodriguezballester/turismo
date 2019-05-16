f5d5d70a39a66b39c98e6aef238e9901635eade0
34791843l <34791843l@2A1PC23.mshome.net>
2019-05-16 12:59:15 +0200

    Merge origin/master
    Conflicts:
    	src/Vista/Administrador/Principal/PrincipalAdminController.java

--- src/Vista/Usuario/UsuarioController.java
+++ src/Vista/Usuario/UsuarioController.java
@@ -97,9 +97,9 @@ public class UsuarioController implements Initializable {
     @FXML
     private void logearse(ActionEvent event) {
         // Utilizar uno de estos tres metodos
-        logearseBueno();
-//             logearseComoCliente();
-//        logearseComoAdministrador();
+ //       logearseBueno();
+             logearseComoCliente();
+ //      logearseComoAdministrador();
     }
     
     @FXML
